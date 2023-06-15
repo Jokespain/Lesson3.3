74ff2df392651bd3c1397bb513af006989568649
J0ke <cs.gamer0902@gmail.com>
2023-06-15 21:12:40 +0300

    First commit

--- src/Cat.java
+++ src/Cat.java
@@ -1,6 +1,6 @@
 public class Cat extends Animal {
     @Override
-    public void Description (String sound, String name, int age) {
+    public void Description(String sound, String name, int age) {
         super.Description(sound, name, age);
     }
 }
