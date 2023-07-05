71e591fe438bbd887dcb267b9558d8bce698ca0d
J0ke <cs.gamer0902@gmail.com>
2023-07-05 13:26:06 +0300

    Lesson 5 (HomeWork5)

--- src/Animal.java
+++
@@ -1,5 +0,0 @@
-public class Animal {
-    public void Description(String sound, String name, int age) {
-        System.out.println("Звук - " + sound + ", Имя - " + name + ", Возраст - " + age);
-    }
-}
--- src/Dog.java
+++
@@ -1,6 +0,0 @@
-public class Dog extends Animal {
-    @Override
-    public void Description(String sound, String name, int age) {
-        super.Description(sound, name, age);
-    }
-}
